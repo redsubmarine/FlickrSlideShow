f6d381bcd6bd1a088ecb82d7317bc619482a1fa2
red <seokz20@gmail.com>
2018-10-12 20:24:44 +0900

    Initial Commit

---
+++ FlickrSlideShow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrSlideShow
+//
+//  Created by 양원석 on 12/10/2018.
+//  Copyright © 2018 red. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
