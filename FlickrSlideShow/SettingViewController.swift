7a2bda70b2dbbba21f798a336029e9ea9539679d
red <seokz20@gmail.com>
2018-10-13 00:01:59 +0900

    [dev] 슬라이드 쇼 dissolve 애니메이션 효과 확인.
    서버 연동하기 전 로컬 샘플 이미지를 통해 애니메이션 전환 효과를 빠르게 구현함.

---
+++ FlickrSlideShow/SettingViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SettingViewController.swift
+//  FlickrSlideShow
+//
+//  Created by 양원석 on 12/10/2018.
+//  Copyright © 2018 red. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class SettingViewController: UITableViewController {
+
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timeSlider: UISlider!
+
+    var disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        timeSlider.isContinuous = false
+
+        timeSlider.rx.controlEvent(.valueChanged)
+            .bind(onNext: reloadTimeLabel)
+            .disposed(by: disposeBag)
+    }
+
+    func reloadTimeLabel() {
+        let time = Int(timeSlider.value)
+
+        timeLabel.text = "\(time) sec"
+    }
+
+}
---
+++ FlickrSlideShow/SlideShowViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SlideShowViewController.swift
+//  FlickrSlideShow
+//
+//  Created by 양원석 on 12/10/2018.
+//  Copyright © 2018 red. All rights reserved.
+//
+
+import UIKit
+
+class SlideShowViewController: UIViewController {
+
+    @IBOutlet weak var photoView: UIImageView!
+
+    var viewModel: SlideShowViewModel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        photoView.image = viewModel.currentImage
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+        let currentImage = viewModel.currentImage
+        viewModel.next()
+        let nextImage = viewModel.currentImage
+
+        let dissolve = CABasicAnimation(keyPath: "contents")
+        dissolve.duration = 1.0
+        dissolve.fromValue = currentImage.cgImage
+        dissolve.toValue = nextImage.cgImage
+//        dissolve.isRemovedOnCompletion = false
+
+        photoView.layer.add(dissolve, forKey: "animateDissolve")
+        photoView.image = nextImage
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
