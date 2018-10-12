a30599af2b91700e2f9c979f6daeb15a92be9336
red <seokz20@gmail.com>
2018-10-13 00:01:59 +0900

    [dev] 슬라이드 쇼 dissolve 애니메이션 효과 확인.
    서버 연동하기 전 로컬 샘플 이미지를 통해 애니메이션 전환 효과를 빠르게 구현함.

--- FlickrSlideShow/AppDelegate.swift
+++ FlickrSlideShow/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let slideShowViewController = window?.rootViewController as? SlideShowViewController
+
+        slideShowViewController?.viewModel = SlideShowViewModel()
+
         return true
     }
 
---
+++ FlickrSlideShow/SlideShowViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  SlideShowViewModel.swift
+//  FlickrSlideShow
+//
+//  Created by 양원석 on 12/10/2018.
+//  Copyright © 2018 red. All rights reserved.
+//
+
+import UIKit
+
+struct SlideShowViewModel {
+    var photos: [UIImage]
+    var currentIndex: Int
+
+    init() {
+        photos = [
+            UIImage(named: "30331203967_7720cb9c89_m")!,
+            UIImage(named: "30331205227_c07c0e139a_m")!,
+            UIImage(named: "30331205497_c93c763c59_m")!,
+            UIImage(named: "30331205857_2df4c2c643_m")!,
+            UIImage(named: "30331208127_263fcdae5a_m")!
+        ]
+
+        currentIndex = 0
+    }
+
+    var currentImage: UIImage {
+        return photos[currentIndex]
+    }
+
+    mutating func next() {
+        currentIndex = currentIndex == photos.count - 1 ? 0 : currentIndex + 1
+    }
+}
